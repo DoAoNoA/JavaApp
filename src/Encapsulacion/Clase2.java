6cb542df1bab6964038ee9ecbc5b633405a5e60c
Dana <aguirre.daniela010@gmail.com>
2018-06-04 16:08:51 -0300

    Ejemplo para ver como funciona el encpsulamiento.

---
+++ src/Encapsulacion/Clase2.java
@@ -0,0 +1,10 @@
+package Encapsulacion;
+
+public class Clase2 {
+    public static void main(String[] args) {
+        Clase1 objeto1 = new Clase1();
+
+        objeto1.setEdad(20);
+        System.out.println("La edad es: " + objeto1.getEdad());
+    }
+}
